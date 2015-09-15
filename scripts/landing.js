76891fd8a2451ffbb4c74c0fa5f0124860807bb0
Sanny Lin <sanny@bloc.io>
2015-09-15 11:05:38 -0700

    Refactored revealPoint animation

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,28 +1,14 @@
 var animatePoints = function() {
     var points = document.getElementsByClassName('point');
 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";   
+    function revealPoint(p){
+        for(i=0; i<p.length; i++){
+            p[i].style.opacity = 1;
+            p[i].style.transform = "scaleX(1) translateY(0)";
+            p[i].style.msTransform = "scaleX(1) translateY(0)";
+            p[i].style.WebkitTransform = "scaleX(1) translateY(0)";  
+        }
     };
 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";   
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    revealPoint(points);
 };
