89127388d80062d6e893874bf61687f16d7acf8f
Sanny Lin <sanny@bloc.io>
2015-10-01 22:34:17 -0700

    Checkpoint 26 - DOM Scripting: Play/Pause Button, Complete

--- scripts/album.js
+++ scripts/album.js
@@ -45,7 +45,7 @@ var albumSanny = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
           '<tr class="album-view-song-item">'
-        + '  <td class="song-item-number">' + songNumber + '</td>'
+        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
         + '  <td class="song-item-title">' + songName + '</td>'
         + '  <td class="song-item-duration">' + songLength + '</td>'
         + '</tr>'
@@ -72,9 +72,25 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
     }
 };
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+
+    for (i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function(event) {
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
 
     var albums = [albumMarconi, albumSanny, albumPicasso];
     var albumArt = document.getElementsByClassName('album-cover-art')[0];
