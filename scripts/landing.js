310eaece2fd31d211295d8ab44a93eb1f540eccb
Sanny Lin <sanny@bloc.io>
2015-10-13 16:50:53 -0700

    29 jQuery Landing Page complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
-function revealPoint(p){
-    p.style.opacity = 1;
-    p.style.transform = "scaleX(1) translateY(0)";
-    p.style.msTransform = "scaleX(1) translateY(0)";
-    p.style.WebkitTransform = "scaleX(1) translateY(0)";  
-}
+var animatePoints = function() {
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
 
-function animatePoints(p) {  
-    forEach(p, revealPoint);
+    $.each($('.point'), revealPoint);
 };
 
 $(window).load(function() {
